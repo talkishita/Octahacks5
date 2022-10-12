284b5f87e41594eb4637b8946e8efcad869d8051
YAKSHIT-22 <yakshitgarg25@gmail.com>
2022-10-12 15:56:13 +0530

    Added Prizes

--- src/sections/Prizes.jsx
+++ src/sections/Prizes.jsx
@@ -5,14 +5,14 @@ function Prizes() {
     <div id="prizes" className='min-h-screen bg-slate-900 bg-[url(https://github.com/ikaushiksharma/WhatsApp_Ui/blob/master/backVideo2.gif?raw=true)] bg-no-repeat bg-contain grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-full xl:h-[84%] lg:h-[84%] h-[88%] p-2'>
       <div className='flex flex-col justify-center items-center backdrop-blur-lg bg-black/30'>
         <div className='p-2 flex flex-col justify-center items-center'>
-          <div className='md:w-full lg:w-[88%] h-full flex flex-col justify-center items-start text-white bg-transparent mb-[4px] cursor-pointer space-y-4'>
+          <div className='md:w-full lg:w-[88%] h-full flex flex-col justify-center items-center md:items-start text-white bg-transparent mb-[4px] cursor-pointer space-y-4'>
             <h2 className='text-[1.2rem] lg:text-[1.3rem] xl:text-[1.6rem] font-medium'>
               'TECH IT EASY'
             </h2>
             <h1 className='text-4xl lg:text-5xl xl:text-6xl font-normal text-transparent bg-gradient-to-r from-[#6c6aff] via-[#fbc7d4] to-[#ff658b] inline-block bg-clip-text'>
               Prizes & Rewards
             </h1>
-            <p className='text-[1.2rem] lg:text-[1.3rem] xl:text-[1.5rem] font-light'>
+            <p className='text-[1.2rem] lg:text-[1.3rem] xl:text-[1.5rem] font-light md:text-left text-center'>
               Just to incentivise your hard work and effort over those 36 hours
               we have prizes, cash rewards and more lined up for the top
               performers by our generous Sponsors. Swags and goodies are for
@@ -23,11 +23,29 @@ function Prizes() {
         </div>
       </div>
       <div className='flex flex-col justify-center items-center backdrop-blur-lg bg-black/30'>
-        <div className='p-2 flex flex-col justify-center items-center'>
-          <div className='prize_section_card wrap-image w-full h-full flex flex-col justify-center items-center text-white bg-transparent border-[2px] mb-[4px] border-orange-400 rounded-[96% 4% 95% 5% / 3% 95% 5% 97%] p-[10px] cursor-pointer'>
-            <h2 className='text-xl font-semibold p-[2%] '>Winner</h2>
-            <h1 className='text-[4rem] font-black'>₹ 15,000 *</h1>
-            <p className='text-[1.2rem]'>+Goodies worth upto</p>
+        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-full h-full justify-center items-center p-2'>
+          <div className='w-full h-full flex flex-col justify-center items-center text-white bg-transparent p-2 cursor-pointer'>
+            <h2 className='text-xl font-semibold p-[2%]'>Winner</h2>
+            <h1 className='text-[2rem] font-black'>₹ 15,000 *</h1>
+            <p className='text-[1rem]'>+Goodies worth upto</p>
+            <p>₹ 1 Lakh</p>
+          </div>
+          <div className='w-full h-full flex flex-col justify-center items-center text-white bg-transparent p-2 cursor-pointer'>
+            <h2 className='text-xl font-semibold p-[2%]'>Winner</h2>
+            <h1 className='text-[2rem] font-black'>₹ 15,000 *</h1>
+            <p className='text-[1rem]'>+Goodies worth upto</p>
+            <p>₹ 1 Lakh</p>
+          </div>
+          <div className='w-full h-full flex flex-col justify-center items-center text-white bg-transparent p-2 cursor-pointer'>
+            <h2 className='text-xl font-semibold p-[2%]'>Winner</h2>
+            <h1 className='text-[2rem] font-black'>₹ 15,000 *</h1>
+            <p className='text-[1rem]'>+Goodies worth upto</p>
+            <p>₹ 1 Lakh</p>
+          </div>
+          <div className='w-full h-full flex flex-col justify-center items-center text-white bg-transparent p-2 cursor-pointer'>
+            <h2 className='text-xl font-semibold p-[2%]'>Winner</h2>
+            <h1 className='text-[2rem] font-black'>₹ 15,000 *</h1>
+            <p className='text-[1rem]'>+Goodies worth upto</p>
             <p>₹ 1 Lakh</p>
           </div>
         </div>
