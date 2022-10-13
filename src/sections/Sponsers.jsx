7e7022322e53a01685e050054a1236116e483767
harshraj29 <rajh3282@gmail.com>
2022-10-13 13:33:58 +0530

    h-screen issue fixed

--- src/sections/Sponsers.jsx
+++ src/sections/Sponsers.jsx
@@ -3,15 +3,17 @@ import React from 'react'
 const Sponsers = () => {
   return (
 
-    <div id="sponsors" className='sm:flex-col flex https://github.com/ikaushiksharma/WhatsApp_Ui/blob/master/backVideo.gif?raw=true bg-contain bg-no-repeat flex-wrap '>
+    <div id="sponsors" className='sm:flex-col flex https://github.com/ikaushiksharma/WhatsApp_Ui/blob/master/backVideo.gif?raw=true bg-contain bg-no-repeat flex-wrap'>
 
 {/* left div----------------------------------------------------------------------------------------- */}
-      <div className='backdrop-blur-lg flex-col md:flex-row flex text-center items-center justify-center'>
+      <div className=' h-screen backdrop-blur-lg flex-col md:flex-row flex text-center items-center justify-center '>
       <div className='w-full'>
         <div className='font-semibold text-5xl text-white pb-4'><h1>Our <span className='font-semibold text-5xl text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Awesome </span>Sponsors</h1></div>
         <div className='text-white'>While all our participants make our event lively. it is our very amazing  <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'> sponsors </span>that <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>make it possible</span></div>
       </div>
       
+{/* right div -----------------------------------------------------------------------------------*/}
+
       <div className='h-full w-full justify-center text-center items-center '>
 
           <div className='flex flex-col text-center h-full items-center justify-center m-4'>
@@ -20,7 +22,7 @@ const Sponsers = () => {
 
           {/* Platinum Sponsors --------------------------------------------------------------------------*/}
 
-          <div className='bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg min-h-[50px] rounded-lg shadow-x overflow-hidden flex-auto m-4 h-auto  w-full pb-4'>
+          <div className='bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg min-h-[50px] rounded-lg shadow-x overflow-hidden flex-auto m-4 h-auto  w-full pb-4 mt-[10%] md:mt-[20%]'>
             <div><h1 className='text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 mt-4'>Platinum Sponsors</h1></div>
 
           {/* Platinum Sponsors Images-------------------------------------------------------------------- */}
@@ -48,7 +50,7 @@ const Sponsers = () => {
 
 
             {/* Premium Sponsors ------------------------------------------------------------------------ */}
-            <div className='bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg min-h-[50px] rounded-lg shadow-x overflow-hidden flex-auto m-4 h-auto w-full pb-4'><div><h1 className='text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 mt-4'>Premium Sponsors</h1></div>
+            <div className='bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg min-h-[50px] rounded-lg shadow-x overflow-hidden flex-auto m-4 h-auto w-full pb-4 mb-[20%]'><div><h1 className='text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 mt-4'>Premium Sponsors</h1></div>
 
             {/* Premium Sponsors Images ---------------------------------------------------------------- */}
           <div className='flex justify-evenly mt-6 flex-wrap items-center'>
